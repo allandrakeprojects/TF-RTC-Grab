6f1ce872f96c29e97a9a314d172d3f7261999868
SSIMAKATI\adulay <allandulay69@gmail.com>
2018-11-15 10:19:17 +0800

    Initial commit

--- TF RTC Grab/Program.cs
+++ TF RTC Grab/Program.cs
@@ -16,7 +16,7 @@ namespace TF_RTC_Grab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Form());
         }
     }
 }
