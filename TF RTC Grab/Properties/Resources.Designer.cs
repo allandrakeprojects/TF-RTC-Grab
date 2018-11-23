8d250b71bbb293569099e8afdb5eb476eac8d973
SSIMAKATI\adulay <allandulay69@gmail.com>
2018-11-23 15:26:11 +0800

    Update

--- TF RTC Grab/Properties/Resources.Designer.cs
+++ TF RTC Grab/Properties/Resources.Designer.cs
@@ -108,15 +108,5 @@ namespace TF_RTC_Grab.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon rtc_tf1 {
-            get {
-                object obj = ResourceManager.GetObject("rtc_tf1", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
