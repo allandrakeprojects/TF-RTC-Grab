f4347199cb069edabe865139f2e6ee352c2b0a6a
SSIMAKATI\adulay <allandulay69@gmail.com>
2018-12-13 10:36:36 +0800

    Update ----------

--- TF RTC Grab/Main_Form.cs
+++ TF RTC Grab/Main_Form.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -1497,6 +1498,24 @@ namespace TF_RTC_Grab
                         ___GetLastRegisteredPlayer();
                     }
                 }
+            }
+        }
+
+        private void timer_flush_memory_Tick(object sender, EventArgs e)
+        {
+            FlushMemory();
+        }
+
+        public static void FlushMemory()
+        {
+            Process prs = Process.GetCurrentProcess();
+            try
+            {
+                prs.MinWorkingSet = (IntPtr)(300000);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception();
             }
         }
     }
